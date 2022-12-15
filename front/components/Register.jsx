966a664feeaf9be49d19040574522d6ead8003c0
Dan Chavez <superdanglyde.yt@gmail.com>
2022-12-15 13:24:49 +0800

    signup

--- front/components/Register.jsx
+++ front/components/Register.jsx
@@ -3,7 +3,7 @@ function Register() {
     <div className="osahan-signup">
       <div className="osahan-header-nav shadow-sm p-3 d-flex align-items-center bg-danger">
         <h5 className="font-weight-normal mb-0 text-white">
-          <a className="text-danger mr-3" href="/"><i className="icofont-rounded-left"></i></a> Sign in to your account
+          <a className="text-danger mr-3" href="/"><i className="icofont-rounded-left"></i></a> Create an account
         </h5>
       </div>
       <div className="p-3">
@@ -23,6 +23,14 @@ function Register() {
           <button type="submit" className="btn btn-danger btn-block osahanbus-btn mb-3 rounded-1 mt-4">CREATE AN ACCOUNT</button>
           <p className="text-muted text-center small">By signing up, you agree to our terms and privacy policy.</p>
         </form>
+        <div className="sign-or d-flex align-items-center justify-content-center mb-4">
+          <hr className="mr-4"></hr>
+          <p className="text-muted text-center py-2 m-0">OR</p>
+          <hr className="ml-4"></hr>
+        </div>
+        <div className="osahan-signin text-center p-1">
+          <p className="m-0">Already a member? <a href="/login" className="text-danger ml-2">Sign In</a></p>
+        </div>
       </div>
     </div>
   );

287be712787d3ea838db64a3ac025c317e5c2243
Marlon Gacrama Jr <mgacrama_ccs@uspf.edu.ph>
2022-12-15 13:45:18 +0800

    Signup and SignIn Working no Trappings

--- front/components/Login.jsx
+++ front/components/Login.jsx
@@ -1,25 +1,93 @@
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
 function Login() {
-  return(
+  const nav = useNavigate();
+
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const onChangeHandle = (e) => {
+    const newUser = { ...user };
+    newUser[e.target.id] = e.target.value;
+    console.log(newUser);
+    setUser(newUser);
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const login = await axios
+        .post("http://localhost:4000/api/auth/signin", {
+          email: user.email,
+          password: user.password,
+        })
+        .catch((err) => {
+          if (err.response.statusText === "Unauthorized") {
+            incorrectPassword();
+          } else if (err.response.statusText === "Not Found") {
+            userNotFound();
+          } else {
+            blank();
+          }
+        });
+      nav("/");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
     <div className="osahan-signup">
       <div className="osahan-header-nav shadow-sm p-3 d-flex align-items-center bg-danger">
         <h5 className="font-weight-normal mb-0 text-white">
-          <a className="text-danger mr-3" href="/"><i className="icofont-rounded-left"></i></a> Sign in to your account
+          <a className="text-danger mr-3" href="/">
+            <i className="icofont-rounded-left"></i>
+          </a>
+          Sign in to your account
         </h5>
       </div>
       <div className="px-3 pt-3 pb-5">
         <form action="#">
           <div className="form-group">
             <label className="text-muted f-10 mb-1">Email Address</label>
-            <input className="form-control" placeholder="Enter your email address"></input>
+            <input
+              id="email"
+              type="email"
+              className="form-control"
+              placeholder="Enter your email address"
+              onChange={(e) => {
+                onChangeHandle(e);
+              }}
+            ></input>
           </div>
           <div className="form-group">
             <label className="text-muted f-10 mb-1">Password</label>
-            <input className="form-control" placeholder="Enter your password"></input>
+            <input
+              id="password"
+              type="password"
+              className="form-control"
+              placeholder="Enter your password"
+              onChange={(e) => {
+                onChangeHandle(e);
+              }}
+            ></input>
           </div>
           <div className="text-right mb-3">
-            <a href="/forgotpassword" className="text-muted small">Forgot your password?</a>
+            <a href="/forgotpassword" className="text-muted small">
+              Forgot your password?
+            </a>
           </div>
-          <button type="submit" className="btn btn-danger btn-block osahanbus-btn mb-4 rounded-1">SIGN IN</button>
+          <button
+            type="submit"
+            className="btn btn-danger btn-block osahanbus-btn mb-4 rounded-1"
+            onClick={handleLogin}
+          >
+            SIGN IN
+          </button>
         </form>
         <div className="sign-or d-flex align-items-center justify-content-center mb-4">
           <hr className="mr-4"></hr>
@@ -27,7 +95,12 @@ function Login() {
           <hr className="ml-4"></hr>
         </div>
         <div className="osahan-signin text-center p-1">
-          <p className="m-0">Not a member? <a href="/register" className="text-danger ml-2">Sign Up</a></p>
+          <p className="m-0">
+            Not a member?{" "}
+            <a href="/register" className="text-danger ml-2">
+              Sign Up
+            </a>
+          </p>
         </div>
       </div>
     </div>
--- front/components/Register.jsx
+++ front/components/Register.jsx
@@ -1,27 +1,95 @@
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
 function Register() {
-  return(
+  const nav = useNavigate();
+
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+    cpassword: "",
+  });
+
+  const onChangeHandle = (e) => {
+    const newUser = { ...user };
+    newUser[e.target.id] = e.target.value;
+    console.log(newUser);
+    setUser(newUser);
+  };
+
+  const onClickAddSubmit = async (e) => {
+    e.preventDefault();
+    const NewUser = await axios.post(`http://localhost:4000/api/auth/signup`, {
+      email: user.email,
+      password: user.password,
+    });
+    nav("/login");
+
+    setUser({
+      email: "",
+      password: "",
+      validpass: "",
+    });
+  };
+  return (
     <div className="osahan-signup">
       <div className="osahan-header-nav shadow-sm p-3 d-flex align-items-center bg-danger">
         <h5 className="font-weight-normal mb-0 text-white">
-          <a className="text-danger mr-3" href="/"><i className="icofont-rounded-left"></i></a> Sign in to your account
+          <a className="text-danger mr-3" href="/">
+            <i className="icofont-rounded-left"></i>
+          </a>
+          Sign in to your account
         </h5>
       </div>
       <div className="p-3">
         <form action="#">
           <div className="form-group">
             <label className="text-muted f-10 mb-1">Email Address</label>
-            <input className="form-control" placeholder="Enter your email address"></input>
+            <input
+              id="email"
+              type="email"
+              onChange={(e) => {
+                onChangeHandle(e);
+              }}
+              className="form-control"
+              placeholder="Enter your email address"
+            ></input>
           </div>
           <div className="form-group">
             <label className="text-muted f-10 mb-1">Password</label>
-            <input className="form-control" placeholder="Enter your password"></input>
+            <input
+              id="password"
+              type="password"
+              onChange={(e) => {
+                onChangeHandle(e);
+              }}
+              className="form-control"
+              placeholder="Enter your password"
+            ></input>
           </div>
           <div className="form-group">
             <label className="text-muted f-10 mb-1">Re-type Password</label>
-            <input className="form-control" placeholder="Re-type your password"></input>
+            <input
+              id="cpassword"
+              type="password"
+              onChange={(e) => {
+                onChangeHandle(e);
+              }}
+              className="form-control"
+              placeholder="Re-type your password"
+            ></input>
           </div>
-          <button type="submit" className="btn btn-danger btn-block osahanbus-btn mb-3 rounded-1 mt-4">CREATE AN ACCOUNT</button>
-          <p className="text-muted text-center small">By signing up, you agree to our terms and privacy policy.</p>
+          <button
+            onClick={onClickAddSubmit}
+            type="submit"
+            className="btn btn-danger btn-block osahanbus-btn mb-3 rounded-1 mt-4"
+          >
+            CREATE AN ACCOUNT
+          </button>
+          <p className="text-muted text-center small">
+            By signing up, you agree to our terms and privacy policy.
+          </p>
         </form>
       </div>
     </div>
